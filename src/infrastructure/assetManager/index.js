d5d2bd73ed3255c36c2b9804cd7d7b02319baa80
Murat Gözel <murat@gozel.com.tr>
2021-06-10 16:13:31 +0300

    Added has method to assetManager.

--- src/infrastructure/assetManager/index.js
+++ src/infrastructure/assetManager/index.js
@@ -2,6 +2,10 @@ function AssetManager() {
   this.assets = Object.assign({}, window.frondjs.assetManifest || {})
 }
 
+AssetManager.prototype.has = function has(path) {
+  return this.assets.hasOwnProperty(path)
+}
+
 AssetManager.prototype.get = function get(path) {
   if (!this.assets.hasOwnProperty(path)) {
     throw new Error('No such asset.')
